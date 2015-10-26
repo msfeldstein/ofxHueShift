09c921bbb58ca20677944a9a50bd79c3e6377db0
Michael Feldstein <msfeldstein@gmail.com>
2015-10-25 18:56:16 -0700

    Create a gitignore

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ A simple ofShader class that lets you rotate the hues of an image.
 
 hsv2rgb code found from sam hocevar's stackexchange post: http://gamedev.stackexchange.com/a/59808  Thanks Sam!
 
-![Example](/thumbnail.png)
+![Example](/animation.gif)
--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -9,11 +9,14 @@ void ofApp::setup(){
 void ofApp::update(){
 
 }
-
+static float t = 0.0;
 //--------------------------------------------------------------
 void ofApp::draw(){
+    if (t >= 1.0) return;
     shiftShader.begin();
-    shiftShader.setUniform1f("amount", 1.0 * mouseX / ofGetWidth());
+    shiftShader.setUniform1f("amount", t);
     sunsetImage.draw(0, 0);
     shiftShader.end();
+    t += 0.02;
+    ofSaveFrame();
 }
